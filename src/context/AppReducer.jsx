569bb7a9323d0e145a5c4fee097a29528bfccd6f
SantiCamm <santi.cammarano@gmail.com>
2022-01-05 16:41:29 -0300

    Revert "Backend + API functionalities added"
    This reverts commit af89b6db106600ecf3dc0f593f1a61a1572d6201.

---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,23 @@
+// The reducer handles state changes in response to certain actions
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+      transactions: [action.payload, ...state.transactions]
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
